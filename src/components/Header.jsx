7bcbb3b83006642df9b0b4574350a3fdffc8a527
ogeylanioglu <orcungeylanioglu@gmail.com>
2025-08-05 15:48:23 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/C_logo.png";
 const Header = () => (
   <header className="hero">
     <img src={logo} alt="Convene Logo" className="logo" />
-    <h1>Elevate Your Check-In Process</h1>
+    <h1>Convene Check-In</h1>
     <p className="subtitle">
       A seamless, modern experience built for every Convene location.
     </p>
